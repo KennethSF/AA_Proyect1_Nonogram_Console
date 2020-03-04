1c8bcad3efe67fb3aa827b25f758fb562ae56acb
Kenneth SF <kensegfon@gmail.com>
2020-03-04 09:17:37 -0600

    First Commit, the program reads a file and create a matrix for the game

---
+++ NonogramSolver/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using static  NonogramSolver.Text;
+using static NonogramSolver.utilities;
+
+namespace NonogramSolver
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creation of the list that are going to have the ammount of color cells in the column and row
+            List<List<int>> rowList = new List<List<int>>();
+            List<List<int>> columnList = new List<List<int>>();
+            //Path that contains the necessary data to start the game
+            const string path =
+                @"C:\Users\Kenneth SF\OneDrive - Estudiantes ITCR\TEC\2020\I Semestre\Análisis de Algoritmos\Proyectos\Proyecto_1\data.txt";
+            //Reads the file and converts it into two list, one for the rows and the other for the columns
+            ReadFile(path, rowList, columnList);
+            int[,] NonogramMatrix = new int[rowList.Count,columnList.Count ];
+            PrintMaxtrix(NonogramMatrix); 
+        }
+    }
+}
---
+++ NonogramSolver/utilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NonogramSolver
+{
+    public class utilities
+    {
+        public static List<int> ConvertToList(string numbers)
+        {
+            var auxList = new List<int>();
+            var auxString = "";
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == ',')
+                {
+                    auxList.Add(int.Parse(auxString));
+                    auxString = "";
+                }
+                else
+                {
+                    auxString += numbers[i];
+                    if (i == numbers.Length - 1)
+                    {
+                        auxList.Add(int.Parse(auxString));
+                        auxString = "";
+                        break;
+                    }
+                }
+            }
+            return auxList;
+        }
+
+        public static void PrintMaxtrix(int[,] matrix)
+        {
+            var rowLength = matrix.GetLength(0);
+            var colLength = matrix.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", matrix[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
